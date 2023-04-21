97071f00d85235642f830922fba1904525c2d91b
0reilly <oreilly.119@osu.edu>
2023-04-21 18:26:10 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Select a Rust base image
+FROM rust:latest as builder
+
+# Create a new directory for the app
+WORKDIR /usr/src/rocketstats_backend
+
+# Copy the source code
+COPY src ./src
+
+# Copy Cargo.toml and Cargo.lock to the working directory
+COPY Cargo.toml Cargo.lock ./
+
+# Build the application
+RUN cargo build --release
+
+# Use a lightweight base image for the final stage
+FROM debian:buster-slim
+
+# Install required libraries
+RUN apt-get update && \
+    apt-get install -y libpq5 && \
+    rm -rf /var/lib/apt/lists/*
+
+# Copy the binary from the builder stage
+COPY --from=builder /usr/src/rocketstats_backend/target/release/rocketstats_backend /usr/local/bin/rocketstats_backend
+
+# Set the entry point
+ENTRYPOINT ["rocketstats_backend"]
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, post};
+use serde::Deserialize;
+use std::fmt;
+use deadpool_postgres::{Config, Pool};
+use tokio_postgres::NoTls;
+use actix_web::web::Data;
+use log::{info, LevelFilter};
+use env_logger::Builder;
+
+impl fmt::Debug for EventData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EventData")
+            .field("url", &self.url)
+            .field("referrer", &self.referrer)
+            .field("user_agent", &self.device.user_agent)
+            .finish()
+    }
+}
+
+#[derive(Deserialize)]
+struct Device {
+    user_agent: String,
+}
+
+#[derive(Deserialize)]
+struct EventData {
+    url: String,
+    referrer: String,
+    device: Device,
+}
+
+async fn create_pool() -> Pool {
+    let mut cfg = Config::default();
+    cfg.dbname = Some("mydb".to_string());
+    cfg.user = Some("myuser".to_string());
+    cfg.password = Some("mypassword".to_string());
+    cfg.host = Some("localhost".to_string());
+    cfg.port = Some(5432);
+
+    let pool = cfg.create_pool(NoTls).unwrap();
+    pool
+}
+
+async fn save_event_data(pool: &Pool, event_data: &EventData) -> Result<(), tokio_postgres::Error> {
+    let client = pool.get().await.unwrap();
+
+    client
+        .execute(
+            "INSERT INTO event_tracking (url, referrer, user_agent) VALUES ($1, $2, $3)",
+            &[
+                &event_data.url,
+                &event_data.referrer,
+                &event_data.device.user_agent,
+            ],
+        )
+        .await?;
+
+    Ok(())
+}
+
+#[post("/api/tracking/event")]
+async fn handle_event(
+    pool: web::Data<Pool>,
+    event_data: web::Json<EventData>,
+) -> impl Responder {
+    println!("Received event data: {:?}", event_data);
+
+    // Save the event data to the PostgreSQL database
+    match save_event_data(&pool, &event_data).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Error saving event data: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    // Initialize the logger
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
+
+    let pool = create_pool().await;
+
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(pool.clone()))
+            .service(handle_event)
+    })
+        .bind("127.0.0.1:8080")?
+        .run();
+
+    info!("Server started on http://127.0.0.1:8080");
+
+    server.await
+}
