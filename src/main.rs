b1ab6e72ecd86103b0e5e2e52bfe2517cc8d56c6
0reilly <oreilly.119@osu.edu>
2023-04-26 21:44:54 -0400

    added location and save to mongodb

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use sqlx::PgPool;
-use std::env;
-use tide::{Request, Response, StatusCode};
+use async_std::task;
+use mongodb::{bson::doc, options::ClientOptions, Client};
 use serde::Deserialize;
+use tide::{Request, Response, StatusCode};
+use std::env;
+use serde_json::Value;
+use reqwest::Client as ReqwestClient;
+use chrono::{DateTime, Utc};
+use chrono_tz::US::Eastern;
+
 
 #[derive(Debug, Deserialize)]
 struct EventData {
+    domain: String,
     url: String,
     referrer: String,
     device: Device,
@@ -19,42 +26,67 @@ struct Device {
 async fn main() -> tide::Result<()> {
     tide::log::start();
 
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PgPool::connect(&db_url).await?;
+    let mongo_username = env::var("MONGO_USERNAME").expect("MONGO_USERNAME must be set");
+    let mongo_password = env::var("MONGO_PASSWORD").expect("MONGO_PASSWORD must be set");
+    let mongo_host = env::var("MONGO_HOST").expect("MONGO_HOST must be set");
 
-    let mut app = tide::with_state(pool);
+    let mut client_options = ClientOptions::parse(format!("mongodb://{}:{}@{}", mongo_username, mongo_password, mongo_host)).await?;
+    client_options.app_name = Some("rocketstats-backend".to_string());
+    let client = Client::with_options(client_options)?;
+    let db = client.database("rocketstats");
 
-    app.at("/api/tracking/event")
-        .options(handle_options)
-        .post(handle_event);
+    let mut app = tide::new();
 
-    app.at("/static/:file")
-        .get(|req: Request<PgPool>| async move {
-            let file = req.param("file")?;
-            let path = format!("./static/{}", file);
-            let body = async_std::fs::read(path).await?;
-            Ok(Response::builder(StatusCode::Ok).body(tide::Body::from(body)).build())
-        });
-
+    app.at("/api/tracking/event").post(move |req: Request<()>| handle_event(req, db.clone()));
 
     app.listen("0.0.0.0:8080").await?;
     Ok(())
 }
 
-async fn handle_options(_req: Request<PgPool>) -> tide::Result {
-    let mut response = Response::new(StatusCode::Ok);
-    response.insert_header("Access-Control-Allow-Origin", "*");
-    response.insert_header("Access-Control-Allow-Methods", "POST, OPTIONS");
-    response.insert_header("Access-Control-Allow-Headers", "Content-Type");
-    Ok(response)
+async fn fetch_location_data(ip: &str) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
+    let client = ReqwestClient::new();
+    let response = client
+        .get(&format!("http://ip-api.com/json/{}", ip))
+        .send()
+        .await?;
+    let location_data: Value = response.json().await?;
+    Ok(location_data)
 }
 
 
-async fn handle_event(mut req: Request<PgPool>) -> tide::Result {
+async fn handle_event(mut req: Request<()>, db: mongodb::Database) -> tide::Result {
     let event_data: EventData = req.body_json().await?;
 
-    //console log the event data (body is sent as stringified json)
-    println!("{:?}", event_data);
+    let location_data = fetch_location_data(&event_data.ip).await?;
+    let utc_now: DateTime<Utc> = Utc::now();
+    let est_now = utc_now.with_timezone(&Eastern);
+    // Get the city, region (state), and country
+    let city = location_data["city"].as_str().unwrap_or("Unknown");
+    let region = location_data["region"].as_str().unwrap_or("Unknown");
+    let country = location_data["country"].as_str().unwrap_or("Unknown");
+
+    println!(
+        "{} - User Agent: {:?}",
+        est_now.format("%Y-%m-%d %H:%M:%S").to_string(),
+        event_data.device.user_agent
+    );
+    println!("   - Referrer: {:?}", event_data.referrer);
+    println!("   - URL: {:?}", event_data.url);
+
+
+    let events = db.collection("events");
+    let document = doc! {
+        "domain": event_data.domain,
+        "url": event_data.url,
+        "referrer": event_data.referrer,
+        "user_agent": event_data.device.user_agent,
+        "country": country,
+        "region": region,
+        "city": city,
+        "timestamp": est_now.to_rfc3339(),
+    };
+
+    events.insert_one(document, None).await?;
 
     Ok(Response::new(StatusCode::Ok))
 }
