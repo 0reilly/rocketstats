30de6817505a07254e904533a5b60c6a6b4c30eb
0reilly <oreilly.119@osu.edu>
2023-04-29 23:59:09 -0400

    fixed

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use reqwest::Client as ReqwestClient;
 use chrono::{DateTime, Utc};
 use chrono_tz::US::Eastern;
 use anyhow::{Context, Result};
-use tide::prelude::*;
 
 
 #[derive(Debug, Deserialize)]
@@ -40,7 +39,7 @@ async fn main() -> tide::Result<()> {
     let mut app = tide::new();
 
     app.at("/api/tracking/event").post(move |req: Request<()>| handle_event(req, db.clone()));
-    app.at("/static/*").get(tide::serve_dir("static"));
+    app.at("/static/*").serve_dir("static")?;
 
 
     app.listen("0.0.0.0:8080").await?;
